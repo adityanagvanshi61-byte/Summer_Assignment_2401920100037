db7c1760835087c2b381237e6c760410fc6a0364
Aditya Pratap Singh <adityanagvanshi61@gmail.com>
2026-06-19 21:55:30 +0530

    Create find all anagrams in strings.java

---
+++ Week_2/Day_2/find all anagrams in strings.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+        if (s.length() < p.length()) return result;
+
+        int[] pCount = new int[26];
+        int[] window = new int[26];
+
+        
+        for (char c : p.toCharArray()) {
+            pCount[c - 'a']++;
+        }
+
+       
+        for (int i = 0; i < s.length(); i++) {
+            window[s.charAt(i) - 'a']++;
+
+           
+            if (i >= p.length()) {
+                window[s.charAt(i - p.length()) - 'a']--;
+            }
+
+           
+            if (Arrays.equals(window, pCount)) {
+                result.add(i - p.length() + 1);
+            }
+        }
+
+        return result;
+    }
+}
